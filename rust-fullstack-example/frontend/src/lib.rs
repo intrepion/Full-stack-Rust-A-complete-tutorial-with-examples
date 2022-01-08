f1b920f1dc30b686ad326a5c61b01d660fafa037
Oliver Forral <intrepion@gmail.com>
2022-01-08 13:28:04 -0700

    updated lib.rs

--- rust-fullstack-example/frontend/src/lib.rs
+++ rust-fullstack-example/frontend/src/lib.rs
@@ -1,8 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+mod owner;
+mod pet;
+
+pub type Anchor = RouterAnchor<AppRoute>;
+
+struct FullStackApp {}
+
+pub enum Msg {}
+
+#[derive(Switch, Clone, Debug)]
+pub enum AppRoute {
+    #[to = "/app/create-owner"]
+    CreateOwner,
+    #[to = "/app/create-pet/{id}"]
+    CreatePet(i32),
+    #[to = "/app/{id}"]
+    Detail(i32),
+    #[to = "/"]
+    Home,
 }
