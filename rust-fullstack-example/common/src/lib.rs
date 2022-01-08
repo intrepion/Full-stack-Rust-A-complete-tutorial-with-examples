102e94f2f827472a7f43c8fcb79789a32e96ac72
Oliver Forral <intrepion@gmail.com>
2022-01-07 23:24:18 -0700

    made the pet and owner contracts

--- rust-fullstack-example/common/src/lib.rs
+++ rust-fullstack-example/common/src/lib.rs
@@ -1,8 +1,62 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, PartialEq)]
+pub struct Owner {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct OwnerRequest {
+    pub name: String,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct OwnerResponse {
+    pub id: i32,
+    pub name: String,
+}
+
+impl OwnerResponse {
+    pub fn of(owner: Owner) -> OwnerResponse {
+        OwnerResponse {
+            id: owner.id,
+            name: owner.name,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq)]
+pub struct Pet {
+    pub id: i32,
+    pub name: String,
+    pub owner_id: i32,
+    pub animal_type: String,
+    pub color: Option<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct PetRequest {
+    pub name: String,
+    pub animal_type: String,
+    pub color: Option<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct PetResponse {
+    pub id: i32,
+    pub name: String,
+    pub animal_type: String,
+    pub color: Option<String>,
+}
+
+impl PetResponse {
+    pub fn of(pet: Pet) -> PetResponse {
+        PetResponse {
+            id: pet.id,
+            name: pet.name,
+            animal_type: pet.animal_type,
+            color: pet.color,
+        }
     }
 }
