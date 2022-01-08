120d6560571fbaac33e65014a23a886124273348
Oliver Forral <intrepion@gmail.com>
2022-01-08 12:21:29 -0700

    putting code in the correct places

--- rust-fullstack-example/backend/src/db/owner.rs
+++ rust-fullstack-example/backend/src/db/owner.rs
@@ -9,41 +9,29 @@ pub async fn fetch(db_pool: &DBPool) -> Result<Vec<Owner>> {
     Ok(rows.iter().map(|r| row_to_owner(&r)).collect())
 }
 
-pub async fn create(db_pool: &DBPool, owner_id: i32, body: PetRequest) -> Result<Pet> {
+pub async fn fetch_one(db_pool: &DBPool, id: i32) -> Result<Owner> {
     let con = get_db_con(db_pool).await?;
-    let query = format!(
-        "INSERT INTO {} (name, owner_id, animal_type, color) VALUE ($1, $2, $3, $4) RETURNING *",
-        TABLE
-    );
+    let query = format!("SELECT {} FROM {} WHERE id = $1", SELECT_FIELDS, TABLE);
+
     let row = con
-        .query_one(
-            query.as_str(),
-            &[&body.name, &owner_id, &body.animal_type, &body.color],
-        )
+        .query_one(query.as_str(). &[&id])
         .await
         .map_err(DBQueryError)?;
-    Ok(row_to_pet(&row))
+    Ok(row_to_owner(&row))
 }
 
-pub async fn delete(db_pool: &DBPool, owner_id: i32, id: i32) -> Result<u64> {
+pub async fn create(db_pool: &DBPool, body: OwnerRequest) -> Result<Owner> {
     let con = get_db_con(db_pool).await?;
-    let query = format!("DELETE FROM () WHERE id = $1 AND owner_id = $2", TABLE);
-    con.execute(query.as_str(), &[&id, &owner_id])
+    let query = format!("INSERT INTO {} (name) VALUES ($1) RETURNING *", TABLE);
+    let row = con
+        .query_one(query.as_str(), &[&body.name])
         .await
-        .map_err(DBQueryError)
+        .map_err(DBQueryError)?;
+    Ok(row_to_owner(&row))
 }
 
-fn row_to_pet(row: &Row) -> Pet {
+fn row_to_owner(row: &Row) -> Owner {
     let id: i32 = row.get(0);
-    let owner_id: i32 = row.get(1);
-    let name: String = row.get(2);
-    let animal_type: String = row.get(3);
-    let color: Option<String> = row.get(4);
-    Pet {
-        id,
-        name,
-        owner_id,
-        animal_type,
-        color,
-    }
+    let name: String = row.get(1);
+    Owner { id, name }
 }
